216f9135fdf82b8367d86234f378e19f22ecd840
Caio Henrique <caiocoxim10@gmail.com>
2023-02-22 20:58:26 -0400

    feat: add new assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    fontFamily: {
+      'baloo2-regular': ['Baloo2 Regular'],
+      'baloo2-medium': ['Baloo2 Medium'],
+      'baloo2-bold': ['Baloo2 Bold'],
+      'baloo2-semibold': ['Baloo2 SemiBold'],
+      'baloo2-extrabold': ['Baloo2 ExtraBold'],
+    },
   },
   plugins: [],
 };
