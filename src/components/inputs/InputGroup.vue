70fbd2918d4a54aa4c85da2a6cc82b24cde76ab5
Caio Henrique <caiocoxim10@gmail.com>
2023-02-22 22:13:42 -0400

    feat: input group component

---
+++ src/components/inputs/InputGroup.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup>
+import { ref, watch } from 'vue';
+
+type InputGroupProps = {
+  label: string;
+  type: 'email' | 'text' | 'date' | 'number' | 'password' | 'search';
+  modelValue?: string;
+};
+
+const props = defineProps<InputGroupProps>();
+const emit = defineEmits<{
+  (event: 'update:modelValue', value: string | null): void;
+}>();
+
+const inputValue = ref(null);
+
+watch(inputValue, (value) => emit('update:modelValue', value));
+
+const inputId = `${props.label.toLowerCase()}_${Math.floor(Math.random() * 6)}`;
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <label :for="inputId" class="font-baloo2-bold text-neutral-800">
+      {{ props.label }}
+    </label>
+    <input
+      :id="inputId"
+      :type="props.type"
+      v-model="inputValue"
+      class="bg-slate-50 border-neutral-800 h-16 rounded-md shadow-lg outline-none p-2 transition-all duration-300 focus:border-sky-300"
+    />
+  </div>
+</template>
+
+<style scoped>
+input {
+  border-width: 1px;
+}
+</style>
