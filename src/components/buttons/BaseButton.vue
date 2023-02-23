109b913eea011ddcab09d4ab8b1c9a6a4f1d9449
Caio Henrique <caiocoxim10@gmail.com>
2023-02-22 23:29:31 -0400

    feat: base button component

---
+++ src/components/buttons/BaseButton.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+type ButtonProps = {
+  text: string;
+  type: 'button' | 'submit' | 'reset';
+};
+
+const props = defineProps<ButtonProps>();
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <button
+      :type="props.type"
+      class="bg-primary text-light text-lg font-baloo2-bold h-16 rounded-md shadow-lg outline-none transition-all duration-300 hover:bg-primary-darken active:opacity-30"
+    >
+      {{ props.text }}
+    </button>
+  </div>
+</template>
