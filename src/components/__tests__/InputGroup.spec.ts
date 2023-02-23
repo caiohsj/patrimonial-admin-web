2057dc58a2a4f34af15ce5682c8ef4080a37f7d5
Caio Henrique <caiocoxim10@gmail.com>
2023-02-22 21:49:45 -0400

    test: input group test

---
+++ src/components/__tests__/InputGroup.spec.ts
@@ -0,0 +1,23 @@
+import { describe, it, expect } from 'vitest';
+
+import { mount } from '@vue/test-utils';
+import InputGroup from '@/components/inputs/InputGroup.vue';
+
+describe('InputGroup', () => {
+  it('renders properly', async () => {
+    const wrapper = mount(InputGroup, {
+      props: {
+        label: 'Email',
+        type: 'email',
+        modelValue: '',
+        'onUpdate:modelValue': (e: string) => {
+          wrapper.setProps({ modelValue: e });
+        },
+      },
+    });
+
+    await wrapper.find('input').setValue('meu@email.com');
+
+    expect(wrapper.props('modelValue')).toBe('meu@email.com');
+  });
+});
