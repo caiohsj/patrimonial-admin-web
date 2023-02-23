ae8c7d8f6810f656c21373f7bba2b67875a9e65c
Caio Henrique <caiocoxim10@gmail.com>
2023-02-22 20:59:31 -0400

    feat: remove old routes and add login route

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,18 +1,11 @@
-import HomeView from '../views/HomeView.vue';
+import LoginView from '@/views/LoginView.vue';
 
 const routes = [
+  { path: '/', redirect: '/entrar' },
   {
-    path: '/',
-    name: 'home',
-    component: HomeView,
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (About.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import('../views/AboutView.vue'),
+    path: '/entrar',
+    name: 'login',
+    component: LoginView,
   },
 ];
 
---
+++ src/views/LoginView.vue
@@ -0,0 +1,10 @@
+<template>
+  <main class="bg-slate-50 h-screen flex flex-col justify-center items-center">
+    <img
+      src="@/assets/images/logo.png"
+      alt="SMX Inventários"
+      class="w-48 h-40"
+    />
+    <h1 class="font-baloo2-bold text-gray-400">Autenticação</h1>
+  </main>
+</template>
