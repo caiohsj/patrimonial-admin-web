73452b9e45d00eefa6cc9ab62f5470c1ed39e254
Caio Henrique <caiocoxim10@gmail.com>
2023-02-22 23:28:36 -0400

    style: tailwind colors custom

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
 
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
@@ -10,6 +11,13 @@ module.exports = {
       'baloo2-bold': ['Baloo2 Bold'],
       'baloo2-semibold': ['Baloo2 SemiBold'],
       'baloo2-extrabold': ['Baloo2 ExtraBold'],
+    },
+    colors: {
+      primary: '#F2D022',
+      'primary-darken': '#F2B035',
+      dark: '#262626',
+      light: '#F8FAFC',
+      ...colors,
     },
   },
   plugins: [],
