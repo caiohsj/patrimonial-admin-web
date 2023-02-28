76fcba09871f5824d3734276d2d9aff4094b5a01
Caio Henrique <caiocoxim10@gmail.com>
2023-02-27 20:42:40 -0400

    feat: add current branch and current place for session store

--- src/stores/session.ts
+++ src/stores/session.ts
@@ -1,10 +1,16 @@
 import type { SignInFormData } from '@/@types/interfaces/SignInFormData';
 import { defineStore } from 'pinia';
 import UserResource from '@/api/resources/user';
+import { useBranchStore } from './branch';
+import { usePlaceStore } from './place';
 import type { User } from '@/@types/interfaces/models/user';
+import type { Branch } from '@/@types/interfaces/models/branch';
+import type { Place } from '@/@types/interfaces/models/place';
 
 type SessionStoreState = {
   currentUser: User | null;
+  currentBranch: Branch | null;
+  currentPlace: Place | null;
   token: string | null;
   abilities: Array<string>;
 };
@@ -12,6 +18,8 @@ type SessionStoreState = {
 export const useSessionStore = defineStore('session', {
   state: (): SessionStoreState => ({
     currentUser: null,
+    currentBranch: null,
+    currentPlace: null,
     token: null,
     abilities: [],
   }),
@@ -26,6 +34,16 @@ export const useSessionStore = defineStore('session', {
         token: data.token,
         abilities: data.abilities,
       });
+    },
+
+    setCurrentBranch(index: number) {
+      const branchStore = useBranchStore();
+      this.currentBranch = branchStore.branches[index];
+    },
+
+    setCurrentPlace(index: number) {
+      const placeStore = usePlaceStore();
+      this.currentPlace = placeStore.places[index];
     },
   },
   persist: true,
