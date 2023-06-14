c6c65c797c1d7caef3e8db6f057f23f4615f43fd
Caio <caiocoxim10@gmail.com>
2023-06-13 22:14:38 -0400

    feat: style selectable list

--- src/components/inputs/SelectableList.vue
+++ src/components/inputs/SelectableList.vue
@@ -1,6 +1,4 @@
 <script lang="ts" setup>
-import { computed } from 'vue';
-
 type SelectOption = {
   value: string | number;
   text: string;
@@ -17,16 +15,26 @@ const emit = defineEmits<{
   (event: 'update:modelValue', value: string | number): void;
 }>();
 
-const updateModelValue = (value: string | number) => emit('update:modelValue', value);
+const updateModelValue = (value: string | number) =>
+  emit('update:modelValue', value);
 </script>
 
 <template>
   <div class="flex flex-col">
-    <p class="font-baloo2-bold text-dark">
+    <p class="font-baloo2-bold text-dark" v-if="props.label">
       {{ props.label }}
     </p>
     <ul>
       <li
+        :class="[
+          'cursor-pointer',
+          'rounded-md',
+          'border-b-2',
+          'border-gray-light',
+          'p-2',
+          'hover:bg-success',
+          'hover:text-white',
+        ]"
         v-for="{ value, text } in props.options"
         :key="value"
         @click="updateModelValue(value)"
@@ -37,5 +45,4 @@ const updateModelValue = (value: string | number) => emit('update:modelValue', v
   </div>
 </template>
 
-<style lang="css" scoped>
-</style>
+<style lang="css" scoped></style>
