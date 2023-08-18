5437f69985e2acf568243c51090ce11d43403131
Caio <caiocoxim10@gmail.com>
2023-08-17 20:50:20 -0400

    feat: action for approve user and actions for fetch user by specific filter

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,27 +1,47 @@
 import { defineStore } from 'pinia';
 import type { User } from '@/@types/interfaces/models/user';
 import type { CreateUserFormData } from '@/@types/interfaces/CreateUserFormData';
+import type { UserFilters } from '@/@types/interfaces/api/UserFilters';
 import UserResource from '@/api/resources/user';
 
 type UserStoreState = {
   users: Array<User>;
+  filters: UserFilters;
 };
 
 export const useUserStore = defineStore('user', {
   state: (): UserStoreState => ({
     users: [],
+    filters: {
+      approved: 1,
+    },
   }),
 
   getters: {},
 
   actions: {
     async fetchUsers() {
-      const { data } = await UserResource.index();
+      const { data } = await UserResource.index(this.filters);
       this.users = data;
+    },
+
+    fetchApprovedUsers() {
+      this.filters.approved = 1;
+      this.fetchUsers();
+    },
+
+    fetchDisapprovedUsers() {
+      this.filters.approved = 0;
+      this.fetchUsers();
     },
 
     createUser(params: CreateUserFormData) {
       return UserResource.create(params);
+    },
+
+    async approveUser(id: number) {
+      await UserResource.approve(id);
+      this.fetchApprovedUsers();
     },
 
     createPassword(
