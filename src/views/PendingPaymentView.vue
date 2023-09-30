8bad11fa5a1b2fc0503cf51e3efa309c67f7ae47
Caio <caiocoxim10@gmail.com>
2023-09-30 13:53:04 -0400

    feat: i18n

--- src/views/PendingPaymentView.vue
+++ src/views/PendingPaymentView.vue
@@ -1,42 +1,61 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import WhatsappIcon from '@/components/icons/WhatsappIcon.vue';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+
+const whatsapp = ref('5521981432473');
+const cpf = ref('457.745.760-19');
+const cnpj = ref('52.029.606/0001-53');
+const email = ref('email@smxinventarios.com.br');
 </script>
 
 <template>
   <div class="h-full">
     <h1 class="text-center text-danger font-baloo2-extrabold text-2xl">
-      A sua conta não está aprovada. É necessário fazer um pagamento para aprovar sua conta!
+      {{ t('views.pendingPaymentView.title') }}
     </h1>
     <div class="flex h-full justify-center items-center">
       <div
         class="flex flex-col justify-between p-2 bg-light h-80 w-80 rounded-2xl"
       >
         <div>
-          <h2 class="mb-6 font-baloo2-bold text-center text-lg">Chaves Pix</h2>
+          <h2 class="mb-6 font-baloo2-bold text-center text-lg">
+            {{ t('views.pendingPaymentView.pixKeys.title') }}
+          </h2>
 
           <table class="border-separate border-spacing-2">
             <tr>
-              <td class="rounded-lg px-2 bg-success text-white">CPF</td>
-              <td>457.745.760-19</td>
+              <td class="rounded-lg px-2 bg-success text-white">
+                {{ t('views.pendingPaymentView.pixKeys.cpf') }}
+              </td>
+              <td>{{ cpf }}</td>
             </tr>
             <tr>
-              <td class="rounded-lg px-2 bg-primary text-white">CNPJ</td>
-              <td>52.029.606/0001-53</td>
+              <td class="rounded-lg px-2 bg-primary text-white">
+                {{ t('views.pendingPaymentView.pixKeys.cnpj') }}
+              </td>
+              <td>{{ cnpj }}</td>
             </tr>
             <tr>
-              <td class="rounded-lg px-2 bg-cyan-600 text-white">EMAIL</td>
-              <td>email@smxinventarios.com.br</td>
+              <td class="rounded-lg px-2 bg-cyan-600 text-white">
+                {{ t('views.pendingPaymentView.pixKeys.email') }}
+              </td>
+              <td>{{ email }}</td>
             </tr>
           </table>
         </div>
 
         <a
           class="px-4 bg-green-300 text-white flex h-12 justify-center items-center rounded-lg"
-          href="http://wa.me/5521981432473"
+          :href="`http://wa.me/${whatsapp}`"
           target="_blank"
         >
           <WhatsappIcon class="w-6 h-6 mr-2" />
-          <span class="font-baloo2-bold text-lg">Entre em contato</span>
+          <span class="font-baloo2-bold text-lg">
+            {{ t('views.pendingPaymentView.contact') }}
+          </span>
         </a>
       </div>
     </div>
